ba59aaa8ff8cc93fb43a8e5dc6b323cd886ad76d
范李林 <fanlilin@fanlilindeMacBook-Pro-2.local>
2019-07-26 19:12:54 +0800

    1.0.5 1. 优化代码

--- Sources/Commmand/MMACommand.m
+++ Sources/Commmand/MMACommand.m
@@ -27,4 +27,18 @@
     return data;
 }
 
+- (id)mutableCopyWithZone:(NSZone *)zone
+{
+    MMACommand *command = [[[self class] allocWithZone:zone] init];
+    command.msgType = self.msgType;
+    command.fromId = self.fromId;
+    command.toId = self.toId;
+    command.params = self.params;
+    command.paramsArray = self.paramsArray;
+    command.msgId = self.msgId;
+    command.enabledAck = self.enabledAck;
+    command.serialNo = self.serialNo;
+    return command;
+}
+
 @end
